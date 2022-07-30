92361690b5cb8cf67078f86dd988a64b5620eea5
apaezp <andrea.paez86@gmail.com>
2022-07-29 21:48:01 -0400

    again

--- src/components/pages/HarryPotter/QuoteAndAuthor.js
+++ src/components/pages/HarryPotter/QuoteAndAuthor.js
@@ -16,7 +16,7 @@ class QuoteAndAuthor extends React.Component {
         >
           <h1 id="quote">"{this.props.quote}"</h1>
           <h5 id="author">
-            -{this.props.author ? this.props.author : "Unknown"}-
+            --{this.props.author ? this.props.author : "Unknown"}.
           </h5>
         </div>
         <button
