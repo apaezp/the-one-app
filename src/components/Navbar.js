0ba58e92e0dfce523e84c23dbe38cbf417a25e54
AndresAMF <andres.1991.munoz@gmail.com>
2022-08-01 13:44:49 -0400

    se agrega nombre de repo a link de navbar home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,7 @@ function Navbar() {
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              <Link to='the-one-app/' className='nav-links' onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
