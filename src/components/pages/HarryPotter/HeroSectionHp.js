f40bf4be01bf39ee1d3264fcd3f7c8eb6c018fa1
AndresAMF <andres.1991.munoz@gmail.com>
2022-08-01 18:08:27 -0400

    fixes mobile

--- src/components/pages/HarryPotter/HeroSectionHp.js
+++ src/components/pages/HarryPotter/HeroSectionHp.js
@@ -1,57 +1,53 @@
-import React from 'react'
-import'./HeroSectionHp.css';
+import React from "react";
+import "./HeroSectionHp.css";
 import { Button } from "../../Button";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 export default function HeroSectionHp() {
   return (
-  <div className="hero-containerHp"> 
-          
-          <h1>“We are only as strong as we are united, as weak as we are divided.”</h1>
-          <p>— Albus Dumbledore.</p>
-          <div className="hero-btns">
-          <Link to='./CharactersHp' className='btn-mobile'>
-            <Button
-              className="btns"
-              buttonStyle="btn--outline"
-              buttonSize="btn--large"
-              
-            >
-              Characters
-            </Button>
-          </Link>
-          <Link to='./BooksHp' className='btn-mobile'>
-            <Button
-              className="btns"
-              buttonStyle="btn--outline"
-              buttonSize="btn--large"
-            >
-              Books
-            </Button>
-            </Link>
-            <Link to='./MoviesHp' className='btn-mobile'>
-            <Button
-              className="btns"
-              buttonStyle="btn--outline"
-              buttonSize="btn--large"
-            >
-              Movies
-            </Button>
-            </Link>
-            <Link to='./FooterHp' className='btn-mobile'>
-            <Button
-              className="btns"
-              buttonStyle="btn--outline"
-              buttonSize="btn--large"
-            >
-              Quotes
-            </Button>
-            </Link>
-            
-          </div>
-        
-      
+    <div className="hero-containerHp">
+      <h1>
+        “We are only as strong as we are united, as weak as we are divided.”
+      </h1>
+      <p>— Albus Dumbledore.</p>
+      <div className="hero-btns">
+        <Link to="./CharactersHp" className="btn-mobile">
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+          >
+            Characters
+          </Button>
+        </Link>
+        <Link to="./BooksHp" className="btn-mobile">
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+          >
+            Books
+          </Button>
+        </Link>
+        <Link to="./MoviesHp" className="btn-mobile">
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+          >
+            Movies
+          </Button>
+        </Link>
+        <Link to="./FooterHp" className="btn-mobile">
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+          >
+            Quotes
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
